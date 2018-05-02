1b42f1c82b10929d459050c51dbe42821b317832
Kuba Odias <kuba.odias@relayr.io>
2018-05-02 22:24:43 +0200

    Initial commit of parse_utils

---
+++ README.md
@@ -0,0 +1,5 @@
+# parse_utils
+
+[![Build Status](https://travis-ci.org/relayr/erl-parse-utils.svg?branch=master)](https://travis-ci.org/relayr/erl-parse-utils)
+
+Erlang Abstract Form manipulating functions.
---
+++ src/parse_utils.erl
@@ -0,0 +1,68 @@
+%%------------------------------------------------------------------------------
+%% @author kuba.odias
+%% @copyright relayr 2009-2018
+%% @doc Miscellaneous functions for parsing of Erlang code.
+%% @end
+%%------------------------------------------------------------------------------
+-module(parse_utils).
+
+%%------------------------------------------------------------------------------
+%% Include files
+%%------------------------------------------------------------------------------
+
+%%------------------------------------------------------------------------------
+%% Function exports
+%%------------------------------------------------------------------------------
+-export([
+    string_to_exprs/1,
+    string_to_term/1,
+    term_to_string/1,
+    eval_exprs/1,
+    eval_string/1
+]).
+
+%%------------------------------------------------------------------------------
+%% Macros
+%%------------------------------------------------------------------------------
+
+%%------------------------------------------------------------------------------
+%% Records
+%%------------------------------------------------------------------------------
+
+%%------------------------------------------------------------------------------
+%% Types
+%%------------------------------------------------------------------------------
+
+%% =============================================================================
+%% Exported functions
+%% =============================================================================
+
+-spec string_to_exprs(String :: nonempty_string()) -> [erl_parse:abstract_expr()].
+string_to_exprs(String) ->
+    {ok, Scanned, _} = erl_scan:string(String),
+    {ok, Parsed} = erl_parse:parse_exprs(Scanned),
+    Parsed.
+
+-spec string_to_term(String :: nonempty_string()) -> term().
+string_to_term(String) ->
+    {ok, Scanned, _} = erl_scan:string(String ++ "."),
+    {ok, Parsed} = erl_parse:parse_term(Scanned),
+    Parsed.
+
+-spec term_to_string(Term :: term()) -> nonempty_string().
+term_to_string(Term) ->
+    lists:flatten(io_lib:format("~p", [Term])).
+
+-spec eval_exprs(Exprs :: [erl_parse:abstract_expr()]) -> Result :: any().
+eval_exprs(Exprs) ->
+    {value, Result, _Env} = erl_eval:exprs(Exprs, []),
+    Result.
+
+-spec eval_string(String :: string()) -> Result :: any().
+eval_string(String) ->
+    Exprs = string_to_exprs(String),
+    eval_exprs(Exprs).
+
+%% =============================================================================
+%% Local functions
+%% =============================================================================
