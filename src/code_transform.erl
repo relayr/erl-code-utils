d24d1c833094ebcdd4515d7092ac7d7f25da6bc4
Kuba Odias <kuba.odias@relayr.io>
2018-05-14 11:31:59 +0200

    Code review refactoring

--- src/code_transform.erl
+++ src/code_transform.erl
@@ -1,41 +1,36 @@
 %%------------------------------------------------------------------------------
 %% @author kuba.odias
 %% @copyright relayr 2009-2018
-%% @doc Functions for traversing of Erlang terms.
+%% @doc Functions for transforming of Erlang terms.
 %% @end
 %%------------------------------------------------------------------------------
 -module(code_transform).
 
 %%------------------------------------------------------------------------------
-%% Include files
-%%------------------------------------------------------------------------------
-
-%%------------------------------------------------------------------------------
 %% Function exports
 %%------------------------------------------------------------------------------
 -export([
-    traverse/1,
-    traverse/2
+    traverse/2,
+
+    simple_transform/2
 ]).
-
-%%------------------------------------------------------------------------------
-%% Macros
-%%------------------------------------------------------------------------------
-
-%%------------------------------------------------------------------------------
-%% Records
-%%------------------------------------------------------------------------------
 
 %%------------------------------------------------------------------------------
 %% Types
 %%------------------------------------------------------------------------------
+-type transform_value_type():: atom | number | pid.
+-type transform_action()    :: list_start | list_stop | binary_start | binary_stop | tuple_start | tuple_stop | {value, transform_value_type()}.
+-type transform_output()    :: {go, integer() | list(), list()} | {stop, list()} | integer() | list().
+
+-type transform_function()  :: fun((Action :: transform_action(), Term :: term()) -> transform_output()).
+
+-export_type([
+    transform_function/0
+]).
 
 %% =============================================================================
 %% Exported functions
 %% =============================================================================
-
-traverse(T) ->
-    traverse(T, fun simple_transform/2).
 
 %%------------------------------------------------------------------------------
 %% @spec traverse(S, F) -> Text
@@ -43,60 +38,15 @@ traverse(T) ->
 %%		S = any()
 %%		F = Fun
 %%		Text = list()
-%% @doc Traverses structure, executes F and returns not flattened list
+%% @doc Traverses structure, executes transformation F and returns list
 %% @end
 %%------------------------------------------------------------------------------
-traverse(L, F) when is_list(L) ->
-    case F(list_start, L ) of
-        {go, Open, [H|T] } ->
-            seq_process(traverse(H,F), T, F, Open, F(list_stop, [H|T]));
-        {stop,Text} -> Text
-    end;
-traverse(T, F) when is_tuple(T) ->
-    case F(tuple_start, T) of
-        {go, Open, [H|Tail] } ->
-            seq_process(traverse(H,F), Tail, F, Open, F(tuple_stop, T));
-        {stop, Text} -> Text
-
-    end;
-traverse(B, F) when is_binary(B) ->
-    case F(binary_start, B ) of
-        {go, Open, [H|T] } ->
-            seq_process(traverse(H,F), T, F, Open, F(binary_stop, B));
-        {stop, Text, _ } -> Text
-    end;
-traverse(A, F) when is_atom(A)->
-    F(atom, A);
-
-traverse(N, F) when is_number(N)->
-    F(number, N);
-
-traverse(P, F) when is_pid(P)->
-    F(pid, P).
-
-%% =============================================================================
-%% Local functions
-%% =============================================================================
-
-tailtraverse([], _F) ->
-    "";
-tailtraverse([H|T], F) ->
-    [ $,
-        ,
-        traverse(H,F) | tailtraverse(T, F)
-    ].
-
-seq_process( "", Tail, F, _Open, _Close) ->
-    traverse(Tail,F);
-
-seq_process( H, Tail, F, Open, Close ) ->
-    [   Open ,
-        [ H  | tailtraverse(Tail,F) ],
-        Close
-    ].
+-spec traverse(T :: term(), F :: transform_function()) -> list().
+traverse(T, F) ->
+    lists:flatten(traverse_and_transform(T, F)).
 
 %%------------------------------------------------------------------------------
-%% @spec simple_transform(Action, Term ) -> Ret
+%% @spec simple_transform(Action, Term) -> Ret
 %% where
 %%		Action = atom()
 %%		Term = any()
@@ -105,6 +55,7 @@ seq_process( H, Tail, F, Open, Close ) ->
 %% @end
 %%------------------------------------------------------------------------------
 
+-spec simple_transform(Action :: transform_action(), Term :: term()) -> transform_output().
 simple_transform(list_start,[]) ->
     {stop, "[]"};
 simple_transform(list_start,L) ->
@@ -115,12 +66,12 @@ simple_transform(list_start,L) ->
 simple_transform(list_stop,_L) ->
     $];
 simple_transform(binary_start, <<>>) ->
-    {stop,"<<>>", []};
+    {stop, "<<>>"};
 simple_transform(binary_start, B) ->
     L = binary_to_list(B),
     case io_lib:printable_list(L) of
-        true -> {stop, [io_lib:format("<<~p>>", [L])], L };
-        false ->{go, "<<", L }
+        true -> {stop, [io_lib:format("<<~p>>", [L])]};
+        false ->{go, "<<", L}
     end;
 simple_transform(binary_stop, _B) ->
     ">>";
@@ -130,10 +81,64 @@ simple_transform(tuple_start, T) ->
     {go, ${,  tuple_to_list(T)};
 simple_transform(tuple_stop, _T) ->
     $};
-simple_transform(atom,A) ->
+simple_transform({value,T},V) ->
+    simple_value_transform(T,V).
+
+-spec simple_value_transform(Type :: transform_value_type(), Value :: atom() | number() | pid()) -> list().
+simple_value_transform(atom, A) ->
     io_lib:write(A);
-simple_transform(number,N) ->
+simple_value_transform(number, N) ->
     io_lib:write(N);
-simple_transform(pid,P) ->
+simple_value_transform(pid, P) ->
     {match, [P1, P2, P3]} = re:run(pid_to_list(P), "<([0-9]+)\.([0-9]+)\.([0-9]+)>", [{capture, [1,2,3], list}]),
     io_lib:format("c:pid(~s,~s,~s)", [P1, P2, P3]).
+
+%% =============================================================================
+%% Local functions
+%% =============================================================================
+
+-spec traverse_and_transform(T :: term(), F :: transform_function()) -> list().
+traverse_and_transform(L, F) when is_list(L) ->
+    case F(list_start, L) of
+        {go, Open, [H|T] } ->
+            seq_process(traverse_and_transform(H,F), T, F, Open, F(list_stop, [H|T]));
+        {stop, Text} -> Text
+    end;
+traverse_and_transform(T, F) when is_tuple(T) ->
+    case F(tuple_start, T) of
+        {go, Open, [H|Tail] } ->
+            seq_process(traverse_and_transform(H,F), Tail, F, Open, F(tuple_stop, T));
+        {stop, Text} -> Text
+    end;
+traverse_and_transform(B, F) when is_binary(B) ->
+    case F(binary_start, B) of
+        {go, Open, [H|T] } ->
+            seq_process(traverse_and_transform(H,F), T, F, Open, F(binary_stop, B));
+        {stop, Text} -> Text
+    end;
+traverse_and_transform(A, F) when is_atom(A)->
+    F({value, atom}, A);
+
+traverse_and_transform(N, F) when is_number(N)->
+    F({value, number}, N);
+
+traverse_and_transform(P, F) when is_pid(P)->
+    F({value, pid}, P).
+
+-spec tail_traverse(L :: list(), F :: transform_function()) -> list().
+tail_traverse([], _F) ->
+    "";
+tail_traverse([H|T], F) ->
+    [ $,
+        ,
+        traverse_and_transform(H,F) | tail_traverse(T, F)
+    ].
+
+seq_process( "", Tail, F, _Open, _Close) ->
+    traverse_and_transform(Tail,F);
+
+seq_process( H, Tail, F, Open, Close ) ->
+    [   Open ,
+        [ H  | tail_traverse(Tail,F) ],
+        Close
+    ].
